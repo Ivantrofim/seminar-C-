d902cb815314ea04ec963a6f9e291d8f2d8f2d5f
Ivan <ivantrofim1992@gmail.com>
2023-02-21 22:16:38 +0300

    added tasks

--- task06/Program.cs
+++ task06/Program.cs
@@ -6,7 +6,6 @@
 // 782 -> 72
 // 918 -> 98
 
-
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число из отрезка 100 - 1000 без второй цифры -> {number}");
 int firstDigit = number / 100;
@@ -14,3 +13,4 @@ int thirdDigit = number % 10;
 int sum = firstDigit * 10 + thirdDigit;
 
 Console.WriteLine(sum);
+
---
+++ task26/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+//тело программы
+int number = Prompt("Введите число ");
+int diget = CheckDigit(number);
+Console.WriteLine($"В числе {number}, {diget} ");
+
+//методы
+int Prompt(string mess)     //метод ввода
+{
+    Console.Write(mess);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int CheckDigit(int num)
+{
+    int result = 0;
+    if (num == 0) return 1;
+    else
+    {
+        while (num != 0)
+        {
+            num = num / 10;
+            result = result + 1;
+        }
+    }
+    return result;
+}
